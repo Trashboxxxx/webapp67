f926cdb09053c27ecc6f53aec509981f5b25d41a
Trashboxxxx <110189881+Trashboxxxx@users.noreply.github.com>
2024-09-05 15:24:48 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+console.log('this is my script file from Javascript');
+console.log('hello javascript');
+console.log('hello thursday');
+document.getElementById("text").innerHTML = "Wasuthorn";
+document.write("hello document write");
+
+window.alert("welcome to my page")
+
